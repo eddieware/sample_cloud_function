36c3b924e0addf292cea329e2a32cabcb449f8d5
eddieware <47250727+eddieware@users.noreply.github.com>
2020-08-15 00:16:57 -0500

    update

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,22 @@
 //const functions = require('firebase-functions');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+
 admin.initializeApp(functions.config().firebase);
-exports.addUser = functions.https.onCall((data, context) => {
-    const users = admin.firestore().collection('users');
-    return users.add({
-        name: data["name"],
-        email: data["email"]
+
+// [START addMessage]
+// Take the text parameter passed to this HTTP endpoint and insert it into 
+// Cloud Firestore under the path /messages/:documentId/original
+// [START addMessageTrigger]
+exports.addMessage = functions.https.onRequest(async (req, res) => {
+    // [END addMessageTrigger]
+      // Grab the text parameter.
+      const original = req.query.text;
+      // [START adminSdkAdd]
+      // Push the new message into Cloud Firestore using the Firebase Admin SDK.
+      const writeResult = await admin.firestore().collection('messages').add({original: original});
+      // Send back a message that we've succesfully written the message
+      res.json({result: `Message with ID: ${writeResult.id} added.`});
+      // [END adminSdkAdd]
     });
-});
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+    // [END addMessage]
